4ed4acf4f563ef946426f6c251f73328a4af5845
SISTLA SAI PALLAVI <83531350+sai-pallavi-2003@users.noreply.github.com>
2022-02-05 16:52:13 +0530

    Create RotateString.cpp

---
+++ Strings/RotateString.cpp
@@ -0,0 +1,55 @@
+/*Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+
+Example 1:
+Input: s = "abcde", goal = "cdeab"
+Output: true
+
+Example 2:
+Input: s = "abcde", goal = "abced"
+Output: false
+*/
+
+
+
+#include<iostream>
+using namespace std;
+bool findGoal(string mainStr, string pattern){
+    int ms = mainStr.size();
+    int p = pattern.size();
+    if(ms!=p){
+        cout<<"not rotation.";
+    }
+    string final = mainStr+mainStr;
+    int f= final.size();
+    int i,j,shifts=0;
+    for(i=0;i<=f-p;i++){
+        int flag=0;
+        for(j=0;j<p;j++){
+            if(final[i+j]!=pattern[j]){
+                flag=1;
+                break;
+            }   
+        }
+        if(flag==0){
+            return true;
+        }
+}
+return false;
+
+}
+int main(){
+    string string,goalstring;
+    cout<<"string: ";
+    cin>>string;
+    cout<<"goal string: ";
+    cin>>goalstring;
+    if(findGoal(string,goalstring)){
+        cout<<"rotation";
+    }
+    else{
+        cout<<"not a rotation";
+    }
+}
