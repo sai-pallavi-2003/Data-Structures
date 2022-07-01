f1833b58352e3e0facea207cb8c2c3b387c119cc
SISTLA SAI PALLAVI <83531350+sai-pallavi-2003@users.noreply.github.com>
2022-07-01 23:04:12 +0530

    Update ReverseString.cpp

--- Strings/ReverseString.cpp
+++ Strings/ReverseString.cpp
@@ -17,3 +17,13 @@ int main(){
     getline(cin,str);
     Reverse(str);
 }
+
+
+/*void reverseStr(string& str)
+{
+    int n = str.length();
+ 
+    // Swap character starting from two corners 
+    for (int i = 0; i < n / 2; i++)
+        swap(str[i], str[n - i - 1]);
+}*/
